dea9448099259cf314fb4f4b91a5ed155fe22682
Tezna05 <juantezna89@gmail.com>
2024-09-11 11:21:46 -0500

    Add files via upload

---
+++ lab2.py
@@ -0,0 +1,8 @@
+class humano:
+    def __init__(self,color,edad,nombre):
+        self.color=color
+        self.sabor=edad
+        self.nombre=nombre
+vegetal1=humano("blanco","19","yeison")
+print(type(humano))
+print(vegetal1.nombre)
